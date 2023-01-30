8c997debce6fb3aac4c02328de16f3b0348c7262
ak1ji <123524853+ak1ji@users.noreply.github.com>
2023-01-30 01:54:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ except URLError as e:
 streamlit.dataframe(my_data_row)
 # Snowflake-related functions
 def get_fruit_load_list():
-   with my_cnx.cursor() as my_cnx
+   with my_cnx.cursor() as my_cur
    my_cur.execute("select * from fruit_load_list")
    returm my_cur.fetchall()
 
@@ -85,7 +85,7 @@ streamlit.stop()
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake (new_fruit)
-    with my_cnx.cursor() as my_cnx
+    with my_cnx.cursor() as my_cur
        my_cur.execute("insert into fruit_load_list values ('from strealit')");
        return "Thanks for adding" + new_fruit  
 
